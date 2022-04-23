2ace13bccba2876c9a8d3b3154715c36e6ce3ea9
LeCongTung <tung.100301@gmail.com>
2022-04-23 14:23:12 +0700

    Sort a price and a letter from cart and product

--- app/src/main/java/com/example/hamina/shows/Show_ListCart.kt
+++ app/src/main/java/com/example/hamina/shows/Show_ListCart.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -47,6 +48,48 @@ class Show_ListCart : AppCompatActivity() {
         listItem.layoutManager = LinearLayoutManager(this)
         cartArrayList = arrayListOf<Cart>()
         getData()
+
+        //            Sort a optional price
+        val btnSortPrice: ImageView = findViewById(R.id.btn_sortprice)
+        var valueSortPrice = "increase"
+        btnSortPrice.setOnClickListener {
+
+            if (valueSortPrice.equals("Descrease")){
+
+                valueSortPrice = "Increase"
+                btnSortPrice.setImageResource(R.drawable.ic_price_increase_black)
+
+                getData()
+
+            }else{
+
+                valueSortPrice = "Descrease"
+                btnSortPrice.setImageResource(R.drawable.ic_price_decrease_black)
+
+                getDataDecrease()
+            }
+        }
+
+        //            Sort a optional from A (or Z)
+        val btnSortAZ: ImageView = findViewById(R.id.btn_sortletter)
+        var valueSortAZ = "FromA"
+        btnSortAZ.setOnClickListener {
+
+            if (valueSortAZ.equals("FromZ")){
+
+                valueSortAZ = "FromA"
+                btnSortAZ.setImageResource(R.drawable.ic_ztoa_black)
+
+                getDataZtoA()
+
+            }else{
+
+                valueSortAZ = "FromZ"
+                btnSortAZ.setImageResource(R.drawable.ic_atoz_black)
+
+                getDataAtoZ()
+            }
+        }
     }
 
     private fun getData() {
@@ -68,6 +111,160 @@ class Show_ListCart : AppCompatActivity() {
 
                         val cart = productSnapshot.getValue(Cart::class.java)
                         cartArrayList.add(cart!!)
+
+                        cartArrayList.sortByDescending {
+
+                            it.total
+                        }
+                        Collections.reverse(cartArrayList)
+                    }
+
+                    listItem.adapter = tradeadapter
+                    tradeadapter.setOnItemClickListener(object : Adapter_Cart.onItemClickListener{
+
+                        override fun onItemClick(position: Int) {
+
+//                            val intent = Intent(this@Show_ListProduct, Show_Detail::class.java)
+//                            intent.putExtra("product", productArrayList[position].name)
+//                            intent.putExtra("info", info)
+//                            intent.putExtra("type", type)
+//                            intent.putExtra("pertype", pertype)
+//                            startActivity(intent)
+//                            overridePendingTransition(R.anim.slide_blur_right, R.anim.slide_appear_left)
+                        }
+                    })
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+
+    private fun getDataDecrease() {
+
+        var tradeadapter = Adapter_Cart(cartArrayList)
+        val info = intent.getStringExtra("info").toString()
+
+        database = FirebaseDatabase.getInstance().getReference("Cart/$info")
+
+        val listItem: RecyclerView = findViewById(R.id.list_item)
+        database.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                cartArrayList.clear()
+                if (snapshot.exists()){
+
+                    for (productSnapshot in snapshot.children){
+
+                        val cart = productSnapshot.getValue(Cart::class.java)
+                        cartArrayList.add(cart!!)
+
+                        cartArrayList.sortByDescending {
+
+                            it.total
+                        }
+                    }
+
+                    listItem.adapter = tradeadapter
+                    tradeadapter.setOnItemClickListener(object : Adapter_Cart.onItemClickListener{
+
+                        override fun onItemClick(position: Int) {
+
+//                            val intent = Intent(this@Show_ListProduct, Show_Detail::class.java)
+//                            intent.putExtra("product", productArrayList[position].name)
+//                            intent.putExtra("info", info)
+//                            intent.putExtra("type", type)
+//                            intent.putExtra("pertype", pertype)
+//                            startActivity(intent)
+//                            overridePendingTransition(R.anim.slide_blur_right, R.anim.slide_appear_left)
+                        }
+                    })
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+
+    private fun getDataAtoZ() {
+
+        var tradeadapter = Adapter_Cart(cartArrayList)
+        val info = intent.getStringExtra("info").toString()
+
+        database = FirebaseDatabase.getInstance().getReference("Cart/$info")
+
+        val listItem: RecyclerView = findViewById(R.id.list_item)
+        database.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                cartArrayList.clear()
+                if (snapshot.exists()){
+
+                    for (productSnapshot in snapshot.children){
+
+                        val cart = productSnapshot.getValue(Cart::class.java)
+                        cartArrayList.add(cart!!)
+
+                        cartArrayList.sortByDescending {
+
+                            it.name
+                        }
+                        Collections.reverse(cartArrayList)
+                    }
+
+                    listItem.adapter = tradeadapter
+                    tradeadapter.setOnItemClickListener(object : Adapter_Cart.onItemClickListener{
+
+                        override fun onItemClick(position: Int) {
+
+//                            val intent = Intent(this@Show_ListProduct, Show_Detail::class.java)
+//                            intent.putExtra("product", productArrayList[position].name)
+//                            intent.putExtra("info", info)
+//                            intent.putExtra("type", type)
+//                            intent.putExtra("pertype", pertype)
+//                            startActivity(intent)
+//                            overridePendingTransition(R.anim.slide_blur_right, R.anim.slide_appear_left)
+                        }
+                    })
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+        })
+
+    }
+
+    private fun getDataZtoA() {
+
+        var tradeadapter = Adapter_Cart(cartArrayList)
+        val info = intent.getStringExtra("info").toString()
+
+        database = FirebaseDatabase.getInstance().getReference("Cart/$info")
+
+        val listItem: RecyclerView = findViewById(R.id.list_item)
+        database.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                cartArrayList.clear()
+                if (snapshot.exists()){
+
+                    for (productSnapshot in snapshot.children){
+
+                        val cart = productSnapshot.getValue(Cart::class.java)
+                        cartArrayList.add(cart!!)
+
+                        cartArrayList.sortByDescending {
+
+                            it.name
+                        }
                     }
 
                     listItem.adapter = tradeadapter
--- app/src/main/java/com/example/hamina/shows/Show_ListProduct.kt
+++ app/src/main/java/com/example/hamina/shows/Show_ListProduct.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -51,6 +52,48 @@ class Show_ListProduct : AppCompatActivity() {
         listItem.layoutManager = GridLayoutManager(this, 2)
         productArrayList = arrayListOf<Product>()
         getData()
+
+        //            Sort a optional price
+        val btnSortPrice: ImageView = findViewById(R.id.btn_sortprice)
+        var valueSortPrice = "increase"
+        btnSortPrice.setOnClickListener {
+
+            if (valueSortPrice.equals("Descrease")){
+
+                valueSortPrice = "Increase"
+                btnSortPrice.setImageResource(R.drawable.ic_price_increase_black)
+
+                getData()
+
+            }else{
+
+                valueSortPrice = "Descrease"
+                btnSortPrice.setImageResource(R.drawable.ic_price_decrease_black)
+
+                getDataDecrease()
+            }
+        }
+
+        //            Sort a optional from A (or Z)
+        val btnSortAZ: ImageView = findViewById(R.id.btn_sortletter)
+        var valueSortAZ = "FromA"
+        btnSortAZ.setOnClickListener {
+
+            if (valueSortAZ.equals("FromZ")){
+
+                valueSortAZ = "FromA"
+                btnSortAZ.setImageResource(R.drawable.ic_ztoa_black)
+
+                getDataZtoA()
+
+            }else{
+
+                valueSortAZ = "FromZ"
+                btnSortAZ.setImageResource(R.drawable.ic_atoz_black)
+
+                getDataAtoZ()
+            }
+        }
     }
 
     private fun getData() {
@@ -67,9 +110,9 @@ class Show_ListProduct : AppCompatActivity() {
         database.addValueEventListener(object : ValueEventListener {
 
             override fun onDataChange(snapshot: DataSnapshot) {
-                productArrayList.clear()
-                if (snapshot.exists()){
 
+                if (snapshot.exists()){
+                    productArrayList.clear()
                     for (productSnapshot in snapshot.children){
 
                         val product = productSnapshot.getValue(Product::class.java)
@@ -101,7 +144,167 @@ class Show_ListProduct : AppCompatActivity() {
             override fun onCancelled(error: DatabaseError) {
                 TODO("Not yet implemented")
             }
+
+
         })
 
+    }
+
+    private fun getDataDecrease() {
+
+        var tradeadapter = Adapter_Product(productArrayList)
+        val type = intent.getStringExtra("type").toString()
+        val pertype = intent.getStringExtra("pertype").toString()
+        val info = intent.getStringExtra("info").toString()
+
+        val nameproduct = type + pertype
+        database = FirebaseDatabase.getInstance().getReference(nameproduct)
+
+        val listItem: RecyclerView = findViewById(R.id.list_item)
+        database.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()){
+                    productArrayList.clear()
+                    for (productSnapshot in snapshot.children){
+
+                        val product = productSnapshot.getValue(Product::class.java)
+                        productArrayList.add(product!!)
+
+                        productArrayList.sortByDescending {
+
+                            it.price
+                        }
+                    }
+
+                    listItem.adapter = tradeadapter
+                    tradeadapter.setOnItemClickListener(object : Adapter_Product.onItemClickListener{
+
+                        override fun onItemClick(position: Int) {
+
+                            val intent = Intent(this@Show_ListProduct, Show_Detail::class.java)
+                            intent.putExtra("product", productArrayList[position].name)
+                            intent.putExtra("info", info)
+                            intent.putExtra("type", type)
+                            intent.putExtra("pertype", pertype)
+                            startActivity(intent)
+                            overridePendingTransition(R.anim.slide_blur_right, R.anim.slide_appear_left)
+                        }
+                    })
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
+    }
+
+
+    private fun getDataAtoZ() {
+
+        var tradeadapter = Adapter_Product(productArrayList)
+        val type = intent.getStringExtra("type").toString()
+        val pertype = intent.getStringExtra("pertype").toString()
+        val info = intent.getStringExtra("info").toString()
+
+        val nameproduct = type + pertype
+        database = FirebaseDatabase.getInstance().getReference(nameproduct)
+
+        val listItem: RecyclerView = findViewById(R.id.list_item)
+        database.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()){
+                    productArrayList.clear()
+                    for (productSnapshot in snapshot.children){
+
+                        val product = productSnapshot.getValue(Product::class.java)
+                        productArrayList.add(product!!)
+
+                        productArrayList.sortByDescending {
+
+                            it.name
+                        }
+                        Collections.reverse(productArrayList)
+                    }
+
+                    listItem.adapter = tradeadapter
+                    tradeadapter.setOnItemClickListener(object : Adapter_Product.onItemClickListener{
+
+                        override fun onItemClick(position: Int) {
+
+                            val intent = Intent(this@Show_ListProduct, Show_Detail::class.java)
+                            intent.putExtra("product", productArrayList[position].name)
+                            intent.putExtra("info", info)
+                            intent.putExtra("type", type)
+                            intent.putExtra("pertype", pertype)
+                            startActivity(intent)
+                            overridePendingTransition(R.anim.slide_blur_right, R.anim.slide_appear_left)
+                        }
+                    })
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
+    }
+
+    private fun getDataZtoA() {
+
+        var tradeadapter = Adapter_Product(productArrayList)
+        val type = intent.getStringExtra("type").toString()
+        val pertype = intent.getStringExtra("pertype").toString()
+        val info = intent.getStringExtra("info").toString()
+
+        val nameproduct = type + pertype
+        database = FirebaseDatabase.getInstance().getReference(nameproduct)
+
+        val listItem: RecyclerView = findViewById(R.id.list_item)
+        database.addValueEventListener(object : ValueEventListener {
+
+            override fun onDataChange(snapshot: DataSnapshot) {
+
+                if (snapshot.exists()){
+                    productArrayList.clear()
+                    for (productSnapshot in snapshot.children){
+
+                        val product = productSnapshot.getValue(Product::class.java)
+                        productArrayList.add(product!!)
+
+                        productArrayList.sortByDescending {
+
+                            it.name
+                        }
+                    }
+
+                    listItem.adapter = tradeadapter
+                    tradeadapter.setOnItemClickListener(object : Adapter_Product.onItemClickListener{
+
+                        override fun onItemClick(position: Int) {
+
+                            val intent = Intent(this@Show_ListProduct, Show_Detail::class.java)
+                            intent.putExtra("product", productArrayList[position].name)
+                            intent.putExtra("info", info)
+                            intent.putExtra("type", type)
+                            intent.putExtra("pertype", pertype)
+                            startActivity(intent)
+                            overridePendingTransition(R.anim.slide_blur_right, R.anim.slide_appear_left)
+                        }
+                    })
+                }
+            }
+            override fun onCancelled(error: DatabaseError) {
+                TODO("Not yet implemented")
+            }
+
+
+        })
     }
 }
